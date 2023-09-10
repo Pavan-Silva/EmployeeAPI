32bf6a7b23984d067797af96aa2c4f7914918928
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2023-09-10 14:41:33 +0530

    Employee API v2 with Spring Data JPA

--- src/main/java/com/example/employeeapi/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/employeeapi/service/EmployeeServiceImpl.java
@@ -1,39 +1,51 @@
 package com.example.employeeapi.service;
 
-import com.example.employeeapi.dao.EmployeeDao;
+import com.example.employeeapi.dao.EmployeeRepository;
 import com.example.employeeapi.entity.Employee;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private final EmployeeDao employeeDao;
-    public EmployeeServiceImpl(EmployeeDao employeeDao) {
-        this.employeeDao = employeeDao;
+    private final EmployeeRepository employeeRepository;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDao.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return employeeDao.findById(id);
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        Employee employee;
+
+        if(result.isPresent()) {
+            employee = result.get();
+
+        } else {
+            throw new RuntimeException("Did not find employee id - " + id);
+        }
+
+        return employee;
     }
 
     @Override
     @Transactional
     public Employee save(Employee employee) {
-        return employeeDao.save(employee);
+        return employeeRepository.save(employee);
     }
 
     @Override
     @Transactional
     public void deleteById(int id) {
-        employeeDao.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
