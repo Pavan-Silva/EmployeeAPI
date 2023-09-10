e3f1cad0e9b5b5c2a58511ed284dc5ac1f2715a8
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2023-09-10 16:56:51 +0530

    Revert back to v2 for better customization

---
+++ src/main/java/com/example/employeeapi/controller/EmployeeController.java
@@ -0,0 +1,58 @@
+package com.example.employeeapi.controller;
+
+import com.example.employeeapi.entity.Employee;
+import com.example.employeeapi.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.findById(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + id);
+        }
+
+        return employee;
+    }
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @PutMapping
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.findById(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + id);
+        }
+
+        employeeService.deleteById(id);
+
+        return "Deleted employee id - " + id;
+    }
+}
---
+++ src/main/java/com/example/employeeapi/service/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.employeeapi.service;
+
+import com.example.employeeapi.dao.EmployeeRepository;
+import com.example.employeeapi.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        Employee employee;
+
+        if (result.isPresent()) {
+            employee = result.get();
+        }
+        else {
+            throw new RuntimeException("Did not find employee id - " + id);
+        }
+
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
