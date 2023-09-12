dfa7ca64d97ea127c7d6c161f2f041c3eceea28e
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2023-09-12 11:42:15 +0530

    Restrict URLs based on roles

--- src/main/java/com/example/employeeapi/security/SecurityConfig.java
+++ src/main/java/com/example/employeeapi/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.example.employeeapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -30,5 +35,23 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(pavan,saranga,suranga);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults()); //Set http basic auth
+        http.csrf(AbstractHttpConfigurer::disable); //CSRF is not needed for stateless REST-Apis
+
+        return http.build();
     }
 }
